00bbd61c45c2ffb1861e29b5c31f3240ef591c49
kowshicPrasanna <59225652+kowshicPrasanna@users.noreply.github.com>
2025-01-20 18:48:09 +0530

    Update product.jsx

--- src/product.jsx
+++ src/product.jsx
@@ -1,17 +1,15 @@
 import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { CartContext } from "./CartContext"; // Import CartContext
+import { CartContext } from "./CartContext"; 
 import bag from "./assets/bag.svg";
 import cartimg from "./assets/cart.svg";
 
 function Product() {
   const [products, setProducts] = useState([]);
-  const { cart, addToCart } = useContext(CartContext); // Use context
+  const { cart, addToCart } = useContext(CartContext); 
 
   const fetchProducts = async () => {
-    const fashionProducts = await fetch(
-      "https://676abe38863eaa5ac0df7bd3.mockapi.io/crud/fashion"
-    );
+    const fashionProducts = await fetch("https://fakestoreapi.com/products");
     const fashionProductResponse = await fashionProducts.json();
     setProducts(fashionProductResponse);
   };
@@ -50,20 +48,32 @@ function Product() {
             return (
               <div
                 key={product.id}
-                className="shadow-lg flex flex-col p-[1rem]"
+                className="shadow-lg flex flex-col p-[1rem] w-[15rem] h-[25rem] justify-between" 
               >
                 <img
-                  className="w-[15rem] h-[16rem]"
+                  className="w-[15rem] h-[16rem] object-cover"
                   src={product.image}
-                  alt=""
+                  alt={product.title} 
                 />
-                <span className="py-[1rem]">{product.name}</span>
-                <span className="flex justify-between">
+                <span
+                  className="py-[1rem] text-wrap break-words text-center h-[4rem] overflow-hidden"
+                  style={{ wordBreak: "break-word" }} 
+                >
+                  {product.title}
+                </span>
+                <span className="flex justify-between items-center mt-[0.5rem]">
                   <p className="text-[0.9rem] text-gray-500">
                     $ {product.price}
                   </p>
-                  <button onClick={() => addToCart(product)}>
-                    <img className="w-[1.5rem]" src={cartimg} alt="" />
+                  <button
+                    onClick={() => addToCart(product)}
+                    className="p-[0.5rem] hover:bg-gray-100 rounded" 
+                  >
+                    <img
+                      className="w-[1.5rem]"
+                      src={cartimg}
+                      alt="Add to cart"
+                    />
                   </button>
                 </span>
               </div>
